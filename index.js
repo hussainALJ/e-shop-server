31fcd85ea81fec79fb8290a53caa35cfe00f8f70
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-02 21:33:07 +0300

    Change require to import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const errorHandler = require("./middlewares/errorHandler");
-require("dotenv").config();
+import express from "express";
+import errorHandler from "./middlewares/errorHandler.js";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +10,6 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Welcome to e-shop-server"));
 
 //Global error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`working on http://localhost:${PORT}`));
