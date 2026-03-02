6c5aaa0182fcd5de60dfb80700fdccd778c9c45b
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-02 20:46:51 +0300

    Create a global error handler

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => res.send("Welcome to e-shop-server"))
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`working on http://localhost:${PORT}`))
+app.get("/", (req, res) => res.send("Welcome to e-shop-server"));
+
+//Global error handler
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`working on http://localhost:${PORT}`));
